94b43e41f3e8bda2f89c97e15bfb1745d2e33b6a
Abhinav tanwar <abhinavtanwar400@gmail.com>
2023-10-09 22:24:40 +0530

    Food generation and functionality

---
+++ Day-8/food.py
@@ -0,0 +1,17 @@
+import turtle
+import random
+
+
+class Food(turtle.Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.goto(random.randint(-250, 250), random.randint(-250, 250))
+
+    def refreshFood(self):
+        self.goto(random.randint(-250, 250), random.randint(-250, 250))
