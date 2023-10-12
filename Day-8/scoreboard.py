a6b493852a998c089fcfed74e63640aaa7eba336
Abhinav tanwar <abhinavtanwar400@gmail.com>
2023-10-12 13:34:24 +0530

    Added all time high score maintaining functionality

--- Day-8/scoreboard.py
+++ Day-8/scoreboard.py
@@ -1,6 +1,11 @@
 import turtle
 
 
+with open("100DayOfCodePython\Day-8\storage.txt", mode="r") as file:
+    allTime_highscore = file.read()
+    allTime_highscore = int(allTime_highscore)
+
+
 class Scoreboard(turtle.Turtle):
 
     def __init__(self):
@@ -9,15 +14,21 @@ class Scoreboard(turtle.Turtle):
         self.color("white")
         self.score = 0
         self.penup()
+        self.highscore = allTime_highscore
         self.goto(0, 280)
-        self.write(arg=f"Current score: {self.score}", move=False, align="center")
+        self.write(arg=f"Current score: {self.score}, HighScore: {self.highscore}", move=False, align="center")
 
     def refreshScore(self):
         self.score += 1
         self.clear()
-        self.write(arg=f"Current score: {self.score}", move=False, align="center")
+        self.write(arg=f"Current score: {self.score}, HighScore: {self.highscore}", move=False, align="center")
 
     def gameOver(self):
         self.clear()
         self.goto(0, 0)
-        self.write(arg=f"Game Over. Final Score: {self.score}", align="center", font=("Arial", 20, "normal"))
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.write(arg=f"Game Over. Final Score: {self.score}, HighScore: {self.highscore}", align="center",
+                   font=("Arial", 20, "normal"))
+        with open("100DayOfCodePython\Day-8\storage.txt", mode="w") as myfile:
+            myfile.write(f"{self.highscore}")
