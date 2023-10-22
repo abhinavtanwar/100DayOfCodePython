8055f57294d8b6ad8470d59cc6a2ad2e5b2817fc
Abhinav tanwar <abhinavtanwar400@gmail.com>
2023-10-22 11:00:19 +0530

    Implemented error handling

--- Day-12/NATO_phonetic_main.py
+++ Day-12/NATO_phonetic_main.py
@@ -1,10 +1,19 @@
 import pandas
 
-filePath = "100DayOfCodePython\Day-12\nato_phonetic_alphabet.csv"
+filePath = "100DayOfCodePython\Day-12\data.csv"
 data = pandas.read_csv(filePath)
-name = input("Enter your string: ").upper()
-print(data)
-letter_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(letter_dict)
-out_list = [letter_dict[letter] for letter in name]
-print(out_list)
+flag = 0
+while flag == 0:
+    name = input("Enter your string: ").upper()
+    # print(data)
+    letter_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+    # print(letter_dict)
+    try:
+        out_list = [letter_dict[letter] for letter in name]
+    except KeyError:
+        print("Enter alphabets only, other characters re not allowed")
+        continue
+    else:    
+        print(out_list)
+        flag = 1
+    
---
+++ Day-12/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+else:
+#     # print(data)
+#     letter_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+#     # print(letter_dict)
+#     out_list = [letter_dict[letter] for letter in name]
+#     print(out_list)
