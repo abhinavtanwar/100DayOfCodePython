03041477160c05f3b563390b9f756717ec0fc7d6
Abhinav tanwar <abhinavtanwar400@gmail.com>
2023-10-25 22:05:49 +0530

    LeetCode Question of the day

---
+++ Day-16/LeetCodeQues.py
@@ -0,0 +1,22 @@
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+# You must write an algorithm that runs in O(n) time and without using the division operation.
+
+# solution:
+class Solution:
+    def productExceptSelf(self, nums):
+        ans = [1 for _ in nums]
+        
+        left = 1
+        right = 1
+        
+        for i in range(len(nums)):
+            ans[i] *= left
+            ans[-1-i] *= right
+            left *= nums[i]
+            right *= nums[-1-i]
+        
+        return ans
+
+obj = Solution()
+print(obj.productExceptSelf([1,4,2,6,4,8]))
